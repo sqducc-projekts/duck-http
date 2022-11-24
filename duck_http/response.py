0fd4cca131d9d11ae5b78d30fa725d9c30609edd
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-11-24 23:43:22 +0300

    added response body to response class

--- duck_http/response.py
+++ duck_http/response.py
@@ -4,16 +4,19 @@ class Response:
   def __init__(
     self,
     status: Status,
-    headers: dict
+    headers: dict,
+    body: str
   ) -> None:
     """The Response class is used to make an HTTP response structure
 
     Args:
       status (Status): This is a status object to define the status code and status text for the response
       headers (dict): This is a dictionary of headers and their values
+      body (str): This is the response body
     """
     self.status = status
     self.headers = headers
+    self.body = body
 
   def convertToResponseString(self) -> str:
     """This function converts self into an HTTP response string
@@ -24,4 +27,4 @@ class Response:
     headersAsStrings: str = ""
     for header in self.headers:
       headersAsStrings += f"{header}: {self.headers[header]}\n"
-    return f"HTTP/1.1 {self.status.code} {self.status.text}\n{headersAsStrings}\nhello"
+    return f"HTTP/1.1 {self.status.code} {self.status.text}\n{headersAsStrings}\n{self.body}"
