1c61aecc14af1ce0fc48934b23eae7b8438be69b
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-11-24 23:08:07 +0300

    forgor to add my classes to __init__.py

--- duck_http/__init__.py
+++ duck_http/__init__.py
@@ -9,3 +9,6 @@ __author__ = "Laith Rafid"
 __license__ = "MIT"
 __copyright__ = "Copyright 2022-present Laith Rafid"
 __version__ = "0.0.1"
+
+from .status import Status
+from .response import Response
